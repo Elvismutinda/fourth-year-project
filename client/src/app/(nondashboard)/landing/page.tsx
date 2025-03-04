45305ac4b1ace37d6d286f126e08cc4422282be4
ElvisMutinda <elvismutinda2@gmail.com>
2025-03-04 18:05:22 +0300

    updated components styling

--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 import { useCarousel } from "@/hooks/use-carousel";
 import { useMediaQuery } from "@/hooks/use-media-query";
 import { Button } from "@/components/ui/button";
-import { topics } from "@/config/topics";
+import { topics, useCases } from "@/config/topics";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
   Dialog,
@@ -59,21 +59,21 @@ const LoadingSkeleton = () => {
 const LandingPage = () => {
   const currentImage = useCarousel({ totalImages: 3 });
   const [open, setOpen] = useState(false);
-  const [selectedTopic, setSelectedTopic] = useState<{
-    id: number;
-    title: string;
-    image: string;
-    description: string;
-  } | null>(null);
+  // const [selectedTopic, setSelectedTopic] = useState<{
+  //   id: number;
+  //   title: string;
+  //   image: string;
+  //   description: string;
+  // } | null>(null);
   const isDesktop = useMediaQuery("(min-width: 768px)");
 
   const handleOpen = (topic: {
-    id: number;
+    // id: number;
     title: string;
     image: string;
     description: string;
   }) => {
-    setSelectedTopic(topic);
+    // setSelectedTopic(topic);
     setOpen(true);
   };
 
@@ -104,7 +104,8 @@ const LandingPage = () => {
             Intelligent research and case retrieval tool
           </h1>
           <p className="text-base text-gray-400 mb-8">
-            Access comprehensive case laws instantly, ensuring accuracy and efficiency in your legal practice.
+            Access comprehensive case laws instantly, ensuring accuracy and
+            efficiency in your legal practice.
             <br />
             Research has never been easier.
           </p>
@@ -136,64 +137,81 @@ const LandingPage = () => {
         </div>
       </motion.div>
       <motion.div
-        initial={{ y: 20, opacity: 0 }}
-        whileInView={{ y: 0, opacity: 1 }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
         transition={{ duration: 0.5 }}
-        viewport={{ amount: 0.3, once: true }}
-        className="mx-auto py-12 mt-10"
+        className="mx-auto py-12"
       >
-        <h2 className="text-2xl text-[#2c3854] font-bold mb-4">
-          Featured Practice Areas
+        <h2 className="text-center text-lg font-semibold text-gray-500 uppercase">
+          The Intelaw Advantage, Time to Over Deliver.
         </h2>
-        <p className="text-[#2c3854] mb-8">
-        Specialized Legal Assistance Across Key Practice Areas.
-        </p>
+        <h1 className="text-center text-4xl font-bold text-gray-900 mt-2">
+          Elevate your legal research with{" "}
+          <span className="text-blue-600">unparalleled</span> clarity
+        </h1>
 
-        <div className="flex flex-wrap gap-4 mb-8">
-          {[
-            // "Employment and Labor Law",
-            // "Litigation and Dispute Resolution",
-            "Family Law",
-            "Corporate and Business Law",
-            "Criminal Law",
-            "Intellectual Property Law",
-            "Real Estate Law",
-            // "Estate Planning and Probate Law",
-            "Tax Law",
-          ].map((tag, index) => (
-            <span
-              key={index}
-              className="px-3 py-1 bg-customgreys-secondarybg rounded-full text-sm"
-            >
-              {tag}
-            </span>
-          ))}
-        </div>
+        <div className="flex flex-col md:flex-row justify-between items-center mt-12 gap-8">
+          {/* Left use cases */}
+          <div className="flex flex-col md:w-1/3 space-y-8">
+            {useCases.slice(0, 2).map((useCase) => (
+              <div key={useCase.id} className="text-center">
+                <h3 className="text-4xl font-semibold text-blue-600">
+                  {useCase.id}
+                </h3>
+                <h4 className="text-lg font-bold text-gray-900">
+                  {useCase.title}
+                </h4>
+                <p className="text-gray-600 mt-2">{useCase.description}</p>
+              </div>
+            ))}
+          </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-          {topics.map((topic) => (
-            <Card
-              key={topic.id}
-              onClick={() => handleOpen(topic)}
-              className="cursor-pointer"
-            >
-              <CardHeader className="text-[#2c3854] ">
-                <CardTitle>{topic.title}</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <Image
-                  src={topic.image}
-                  alt={topic.title}
-                  width={300}
-                  height={200}
-                  className="rounded-md"
-                />
-                <p className="text-sm text-[#2c3854] mt-2 line-clamp-3">
-                  {topic.description}
-                </p>
-              </CardContent>
-            </Card>
-          ))}
+          <Card
+            onClick={() =>
+              handleOpen({
+                title: "Criminal Law",
+                description:
+                  "Criminal law defines offenses against the state and prescribes punishments for those who violate them. It includes laws related to theft, assault, fraud, and other crimes, as well as legal procedures to ensure due process and justice for both the accused and victims.",
+                image: "/assets/images/criminal_law.jpg",
+              })
+            }
+            className="w-full md:w-1/3 p-6 shadow-lg text-center cursor-pointer"
+          >
+            <CardHeader>
+              <CardTitle>Criminal Law</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <Image
+                src="/assets/images/criminal_law.jpg"
+                alt="Criminal Law"
+                width={300}
+                height={200}
+                className="rounded-md"
+              />
+              <p className="text-sm text-[#2c3854] mt-2 line-clamp-3">
+                Criminal law defines offenses against the state and prescribes
+                punishments for those who violate them. It includes laws related
+                to theft, assault, fraud, and other crimes, as well as legal
+                procedures to ensure due process and justice for both the
+                accused and victims.
+              </p>
+            </CardContent>
+          </Card>
+
+          {/* Right use cases */}
+          <div className="flex flex-col md:w-1/3 space-y-8">
+            {useCases.slice(2, 4).map((useCase) => (
+              <div key={useCase.id} className="text-center">
+                <h3 className="text-4xl font-semibold text-blue-600">
+                  {useCase.id}
+                </h3>
+                <h4 className="text-lg font-bold text-gray-900">
+                  {useCase.title}
+                </h4>
+                <p className="text-gray-600 mt-2">{useCase.description}</p>
+              </div>
+            ))}
+          </div>
         </div>
       </motion.div>
 
@@ -201,32 +219,44 @@ const LandingPage = () => {
         <Dialog open={open} onOpenChange={setOpen}>
           <DialogContent className="text-[#2c3854] ">
             <DialogHeader>
-              <DialogTitle>{selectedTopic?.title}</DialogTitle>
+              <DialogTitle>Criminal Law</DialogTitle>
             </DialogHeader>
             <Image
-              src={selectedTopic?.image || "/hero1.jpg"}
-              alt={selectedTopic?.title || "Practice Area"}
+              src={"/assets/images/criminal_law.jpg"}
+              alt={"Criminal Law"}
               width={600}
               height={400}
               className="rounded-md"
             />
-            <p className="mt-4">{selectedTopic?.description}</p>
+            <p className="mt-4">
+              Criminal law defines offenses against the state and prescribes
+              punishments for those who violate them. It includes laws related
+              to theft, assault, fraud, and other crimes, as well as legal
+              procedures to ensure due process and justice for both the accused
+              and victims.
+            </p>
           </DialogContent>
         </Dialog>
       ) : (
         <Drawer open={open} onOpenChange={setOpen}>
           <DrawerContent className="text-[#2c3854] items-center justify-center">
             <DrawerHeader>
-              <DrawerTitle>{selectedTopic?.title}</DrawerTitle>
+              <DrawerTitle>Criminal Law</DrawerTitle>
             </DrawerHeader>
             <Image
-              src={selectedTopic?.image || "/hero1.jpg"}
-              alt={selectedTopic?.title || "Practice Area"}
+              src={"/assets/images/criminal_law.jpg"}
+              alt={"Criminal Law"}
               width={400}
               height={300}
               className="rounded-md"
             />
-            <p className="mt-4 p-4">{selectedTopic?.description}</p>
+            <p className="mt-4 p-4">
+              Criminal law defines offenses against the state and prescribes
+              punishments for those who violate them. It includes laws related
+              to theft, assault, fraud, and other crimes, as well as legal
+              procedures to ensure due process and justice for both the accused
+              and victims.
+            </p>
           </DrawerContent>
         </Drawer>
       )}
