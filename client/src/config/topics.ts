5285b95174abad939fa2af64508f85eebc899087
ElvisMutinda <elvismutinda2@gmail.com>
2025-03-04 18:01:21 +0300

    added use cases for the application

--- client/src/config/topics.ts
+++ client/src/config/topics.ts
@@ -1,65 +1,92 @@
-export const topics = [
-  // {
-  //   id: 1,
-  //   title: "Employment and Labor Law",
-  //   description:
-  //     "A landmark privacy case that determined the limits of personal data collection by corporations.",
-  //   image: "/images/case1.jpg",
-  // },
-  // {
-  //   id: 2,
-  //   title: "Litigation and Dispute Resolution",
-  //   description:
-  //     "A historic case on reproductive rights that set a significant legal precedent in constitutional law.",
-  //   image: "/images/case2.jpg",
-  // },
+// export const topics = [
+//   // {
+//   //   id: 1,
+//   //   title: "Employment and Labor Law",
+//   //   description:
+//   //     "A landmark privacy case that determined the limits of personal data collection by corporations.",
+//   //   image: "/images/case1.jpg",
+//   // },
+//   // {
+//   //   id: 2,
+//   //   title: "Litigation and Dispute Resolution",
+//   //   description:
+//   //     "A historic case on reproductive rights that set a significant legal precedent in constitutional law.",
+//   //   image: "/images/case2.jpg",
+//   // },
+//   // {
+//   //   id: 1,
+//   //   title: "Family Law",
+//   //   description:
+//   //     "Family law governs legal matters related to family relationships, such as marriage, divorce, child custody, adoption, and domestic violence. It aims to protect the rights and responsibilities of individuals within a family unit, ensuring fair resolution of disputes and the welfare of children.",
+//   //   image: "/assets/images/family_law.jpg",
+//   // },
+//   // {
+//   //   id: 2,
+//   //   title: "Corporate and Business Law",
+//   //   description:
+//   //     "Corporate and business law deals with the formation, operation, and regulation of businesses. It encompasses areas such as contracts, mergers and acquisitions, corporate governance, intellectual property protection, and compliance with government regulations to ensure lawful business practices.",
+//   //   image: "/assets/images/business_law.png",
+//   // },
+//   // {
+//   //   id: 3,
+//   //   title: "Criminal Law",
+//   //   description:
+//   //     "Criminal law defines offenses against the state and prescribes punishments for those who violate them. It includes laws related to theft, assault, fraud, and other crimes, as well as legal procedures to ensure due process and justice for both the accused and victims.",
+//   //   image: "/assets/images/criminal_law.jpg",
+//   // },
+//   // {
+//   //   id: 4,
+//   //   title: "Intellectual Property Law",
+//   //   description:
+//   //     "Intellectual property law protects the rights of individuals and businesses over their creative works, inventions, and brand identities. This includes copyrights, patents, trademarks, and trade secrets, ensuring creators have exclusive rights to their innovations and artistic expressions.",
+//   //   image: "/assets/images/intellectual_property_law.jpeg",
+//   // },
+//   // {
+//   //   id: 5,
+//   //   title: "Real Estate Law",
+//   //   description:
+//   //     "Real estate law governs the ownership, use, and transfer of real property. It covers transactions such as buying and selling property, leasing agreements, zoning regulations, property disputes, and land use rights, ensuring legal compliance and protection for property owners and tenants.",
+//   //   image: "/assets/images/real_estate_law.jpg",
+//   // },
+//   // {
+//   //   id: 6,
+//   //   title: "Tax Law",
+//   //   description:
+//   //     "Tax law encompasses the rules and regulations that govern taxation imposed by governments. It covers income tax, corporate tax, property tax, and international taxation, ensuring compliance while addressing issues like tax deductions, exemptions, and legal tax planning strategies.",
+//   //   image: "/assets/images/tax_law.jpeg",
+//   // },
+//   // {
+//   //   id: 8,
+//   //   title: "Estate Planning and Probate Law",
+//   //   description:
+//   //     "A groundbreaking case that established the right to counsel for criminal defendants.",
+//   //   image: "/images/case8.jpg",
+//   // },
+// ];
+
+export const useCases = [
   {
-    id: 1,
-    title: "Family Law",
+    id: "01",
+    title: "Rapid Research, Grounded Results",
     description:
-      "Family law governs legal matters related to family relationships, such as marriage, divorce, child custody, adoption, and domestic violence. It aims to protect the rights and responsibilities of individuals within a family unit, ensuring fair resolution of disputes and the welfare of children.",
-    image: "/assets/images/family_law.jpg",
+      "Conduct lightning fast legal research with reliable references, giving you the confidence to build a rock-solid argument.",
   },
   {
-    id: 2,
-    title: "Corporate and Business Law",
+    id: "02",
+    title: "Chat with Case laws",
     description:
-      "Corporate and business law deals with the formation, operation, and regulation of businesses. It encompasses areas such as contracts, mergers and acquisitions, corporate governance, intellectual property protection, and compliance with government regulations to ensure lawful business practices.",
-    image: "/assets/images/business_law.png",
+      "Access the latest legislation and case laws, engage in Q&A, and get instant summaries all in one place.",
   },
   {
-    id: 3,
-    title: "Criminal Law",
+    id: "03",
+    title: "Upload & Analyze Documents",
     description:
-      "Criminal law defines offenses against the state and prescribes punishments for those who violate them. It includes laws related to theft, assault, fraud, and other crimes, as well as legal procedures to ensure due process and justice for both the accused and victims.",
-    image: "/assets/images/criminal_law.jpg",
+      "Upload legal documents for rapid AI analysis. Uncover key insights and actionable takeaways in seconds.",
   },
   {
-    id: 4,
-    title: "Intellectual Property Law",
+    id: "04",
+    title: "Draft Legal Documents",
     description:
-      "Intellectual property law protects the rights of individuals and businesses over their creative works, inventions, and brand identities. This includes copyrights, patents, trademarks, and trade secrets, ensuring creators have exclusive rights to their innovations and artistic expressions.",
-    image: "/assets/images/intellectual_property_law.jpeg",
+      "Generate professional contracts, agreements, submissions and more with our smart drafting tool.",
   },
-  {
-    id: 5,
-    title: "Real Estate Law",
-    description:
-      "Real estate law governs the ownership, use, and transfer of real property. It covers transactions such as buying and selling property, leasing agreements, zoning regulations, property disputes, and land use rights, ensuring legal compliance and protection for property owners and tenants.",
-    image: "/assets/images/real_estate_law.jpg",
-  },
-  {
-    id: 6,
-    title: "Tax Law",
-    description:
-      "Tax law encompasses the rules and regulations that govern taxation imposed by governments. It covers income tax, corporate tax, property tax, and international taxation, ensuring compliance while addressing issues like tax deductions, exemptions, and legal tax planning strategies.",
-    image: "/assets/images/tax_law.jpeg",
-  },
-  // {
-  //   id: 8,
-  //   title: "Estate Planning and Probate Law",
-  //   description:
-  //     "A groundbreaking case that established the right to counsel for criminal defendants.",
-  //   image: "/images/case8.jpg",
-  // },
 ];
